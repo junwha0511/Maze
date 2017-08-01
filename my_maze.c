a399db29227385d6b5510de21d487be6561656fb
junwha0511 <junwha0511@naver.com>
2017-08-01 10:50:44 +0900

    Add files via upload
    complete

--- my_maze.c
+++ my_maze.c
@@ -1,122 +1,110 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<time.h>
 
 #define size 21
+#define unDefined 0
+#define wall 1
+#define path 2
+#define EAST 3
+#define WEST 4
+#define SOUTH 5
+#define NORTH 6
 
- #define EAST 3
- #define WEST 4
- #define SOUTH 5
- #define NORTH 6
 
-int unDefined = 0;
-int wall=1;
-int path=2;
-
-// int east=3;
-// int west=4;
-// int south=5;
-// int north=6;
 
 int greed[size][size]={0};
 
-// int setDirection(int pathDirection){
-//     int direction = rand()%4+2;
-//     while(1){
-//         if(direction==pathDirection){
-//             direction = rand()%4+2;
-//         }
-//         else{
-//             break;
-//         }
-//     }
-//     return direction;
-// 
 void setWall(){
-    int i=0;
-    for(i=0;i<size;i++){
-        greed[0][i]=wall; //상단라인
-        greed[size-1][i]=wall; //하단라인
-        greed[i][0]=wall; //왼쪽라인
-        greed[i][size-1]=wall; //오른쪽라인
+    int i=0,row,col;
+    for(i=0; i<size; i++){
+        greed[0][i] = wall; //왼쪽라인
+        greed[size-1][i] = wall; //오른쪽라인
+        greed[i][0] = wall; //상단라인
+        greed[i][size-1] = wall; //하단라인
     }
-    for(i=0;i<size;i+=2){
-        for(int j=0;j<size;j+=2){
-            greed[i][j]=wall; //모서리 초기화
+    for(row=0; row<size; row+=2){
+        for(col=0; col<size; col+=2){
+            greed[row][col] = wall; //모서리 초기화
         }
     }
 }
-int* randomDirection(int myDirection){
-    int static directions[3]={0};
-    int i,k;
+void makePath(int myX, int myY, int pathDirection){
+    int directions[3] = {0};
+    int i,j,k;
     for(k=0; k<3; k++){
         while(1){  
-            int n=rand()%4+3;
-            for(i=0;i<3;i++){
-                if(n==directions[i]||n==myDirection){
-                    n=0;
+            int n = rand()%4+3;
+            for(j=0; j<3; j++){
+                if(n==directions[j] || n==pathDirection){
+                    n = 0;
                     break;
                 }
             }
             if(n!=0){
-                directions[k]=n;
-                printf("%d\n",directions[k]);
+                directions[k] = n;
                 break;
             }
         }
     }
-    
-    return directions;
-}
-void makePath(int myX,int myY,int pathDirection){
-    int *directions=randomDirection(pathDirection);
-    for(int i=0;i<3;i++){
-        switch(directions[i]){
-            case EAST:
-                if(myX+2<21){
-                    if(greed[myX+2][myY]==unDefined){
-                        greed[myX+2][myY]=path;
-                        makePath(myX+2,myY,WEST);
-                    }else{ //if(greed[myX+2][myY]==path)
-                        greed[myX+1][myY]=wall;
-                    }
-                }
-                break;
-            case WEST:
-                if(myX-2>0){
-                    if(greed[myX-2][myY]==unDefined){
-                        greed[myX-2][myY]=path;
-                        makePath(myX-2,myY,EAST);
-                    }else{ //if(greed[myX-2][myY]==path)
-                        greed[myX-1][myY]=wall;
-                    }
-                }
-                break;
-            case SOUTH:
-                if(myY-2>0){
-                    if(greed[myX][myY-2]==unDefined){
-                        greed[myX][myY-2]=path;
-                        makePath(myX,myY-2,NORTH);
-                    }else{ //if(greed[myX][myY-2]==path)
-                        greed[myX][myY-1]=wall;
-                    }
-                }
-                break;
-            case NORTH:
-                if(myY+2<21){
-                    if(greed[myX][myY+2]==unDefined){
-                        greed[myX][myY+2]=path;
-                        makePath(myX,myY+2,SOUTH);
-                    }else{ //if(greed[myX][myY+2]==path)
-                        greed[myX][myY+1]=wall;
-                    }
-                }
-                break;
-        }
-    }
+    int checkX, checkY;
+	int wallX, wallY;
+	int nextDir;
+
+	for(i=0; i<3; i++){
+		checkX = myX;
+		checkY = myY;
+		wallX = myX;
+		wallY = myY;
+
+		switch(directions[i]){
+			case EAST:
+				checkX = myX + 2;
+				wallX = myX + 1;
+				nextDir = WEST;
+				break;
+			case WEST:
+				checkX = myX - 2;
+				wallX = myX - 1;
+				nextDir = EAST;
+				break;
+			case SOUTH:
+				checkY = myY - 2;
+				wallY = myY - 1;
+				nextDir = NORTH;
+				break;
+			case NORTH:
+				checkY = myY + 2;
+				wallY = myY + 1;
+				nextDir = SOUTH;
+				break;
+		}
+
+		if(0 < checkX && checkX < size && 0 < checkY && checkY < size){
+			if(greed[checkX][checkY]==unDefined){
+				greed[checkX][checkY] = path;
+				makePath(checkX,checkY,nextDir);
+			}else{
+				greed[wallX][wallY] = wall;
+			}
+		}
+	}
 }
 int main(){
+    srand(time(NULL));
+    int row,col;
     setWall();
     makePath(1,1,SOUTH);
+    greed[2][1]=0;
+    for(row=0;row<size;row++){
+        for(col=0;col<size;col++){
+            if(greed[col][row]==2||greed[col][row]==0){
+                greed[col][row]=0;
+            }
+            printf((greed[col][row]==0)?"0":"1");
+        }
+        printf("\n");
+    }
     return 0;
 }
 /* 21x21칸에서 벽공간 길공간 벽공간 길공간 벽공간의 형태로 되어있다.
